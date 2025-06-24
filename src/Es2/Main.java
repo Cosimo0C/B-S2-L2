72fec4e02cbaea15fb7873ad05fb0029c3dde22b
Cosimo0C <cosimocrupi01@gmail.com>
2025-06-24 18:05:09 +0200

    finish es2

--- src/Es2/Main.java
+++ src/Es2/Main.java
@@ -1,7 +1,31 @@
 package Es2;
 
-public class Main {
-    public static void main(String[] args) {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
 
+public class Main {
+    public static List<Integer> listaRandom(int n){
+        Random random= new Random();
+        List<Integer> listaN =new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            int numero= random.nextInt(101);
+            listaN.add(numero);
+        }
+        Collections.sort(listaN);
+        return listaN;
+    }
+    public static List<Integer> listaSort(List<Integer> array ){
+        List<Integer> listaSor=new ArrayList<>();
+        listaSor.addAll(array);
+        for (int i = array.size()-1; i >= 0; i--) {
+            listaSor.add(array.get(i));
+        }
+        return listaSor;
+    }
+    public static void main(String[] args) {
+        System.out.println(listaRandom(10));
+        System.out.println(listaSort(listaRandom(10)));
     }
 }
